8c713872e702e9b639ade6f1ff0d54d5b1ddf51d
Adrien Tremblay <adrien@adrientremblay.com>
2021-01-23 15:55:57 -0500

    Add Battery

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	60,		0},
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	60,	0},
 
-  {"Temp: ", "sensors | grep 'Package id 0' | sed 's/.*:  //;s/  (.*//'", 60, 0},
+  {"Cpu: ", "sensors | grep 'Package id 0' | sed 's/.*:  //;s/  (.*//'", 60, 0},
+
+  {"Bat: ", "upower -i $(upower -e | grep 'BAT') | grep -E 'to\\ full|percentage' | sed 's/.*percentage:\\s*//'", 60, 0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 
